75e3f56e2ddc668ba85240d41b915369d25f3d82
LAPTOP-1QRG1G1C\cihan <cihangirtuncer184@gmail.com>
2022-06-28 16:38:42 +0300

    [feature] UserCacheManager test completed

--- test/cache/cache_test.dart
+++ test/cache/cache_test.dart
@@ -5,8 +5,8 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 void main() {
   late UserCacheManager userCacheManager;
-  List<User>? _items;
-  List<User>? _itemsAllValues;
+  List<User>? items;
+  List<User>? itemsAllValues;
 
   setUp(() async {
     Hive.init('database');
@@ -14,59 +14,31 @@ void main() {
     await userCacheManager.init();
   });
   group("UserCacheManager testing", () {
-    test(("putItem and getItem method "), () async {
+    test(("putItem and getItem method successful "), () async {
       await userCacheManager.putItem("name", User(name: "cihangir"));
       expect(userCacheManager.getItem("name")?.name.toString(), "cihangir");
     });
 
-    test("getValues method", () {
-      _items = userCacheManager.getValues();
-      expect(_items?.first.name, "cihangir");
+    test("getValues method successful", () {
+      items = userCacheManager.getValues();
+      expect(items?.first.name, "cihangir");
     });
 
-    test("addItems method", () async {
+    test("addItems method successful", () async {
       await userCacheManager.putItem("name2", User(name: "emirhan"));
-      await userCacheManager.addItems(_items);
-      _itemsAllValues = userCacheManager.getValues();
+      await userCacheManager.addItems(items);
+      itemsAllValues = userCacheManager.getValues();
     
-      expect(_itemsAllValues?.last.name, "emirhan");
+      expect(itemsAllValues?.last.name, "emirhan");
     });
-      test("removeItem method", () async {
+      test("removeItem method successful", () async {
       await userCacheManager.removeItem("name2"); 
-           _itemsAllValues = userCacheManager.getValues();   
-          print(_itemsAllValues?.last.name);
-              expect(_itemsAllValues?.last.name, "cihangir");
+           itemsAllValues = userCacheManager.getValues();   
+              expect(itemsAllValues?.last.name, "cihangir");
 
     });
 
   
   });
 }
-/*
-  group(("Hive main examples group"), () {
-    
-    
-    
-    test("Add examples", () async {
-      final box = await Hive.openBox('examples');
-      box.add("cihangir");
-      expect(box.values.first, "cihangir");
-    });
 
-    test(("Put examples"), () async {
-      final themeBox = await Hive.openBox<bool>("theme");
-      await themeBox.put("theme", true);
-      expect(themeBox.get("theme"), true);
-    });
-
-    test("Name box create and put", () async {
-      final boxList = await Hive.openBox<String>('examples2');
-      List<String> items = List.generate(100, (index) => '$index');
-      await boxList.addAll(items);
-      expect(boxList.values.first, '0');
-    });
-
-
-
-  });
-}*/
